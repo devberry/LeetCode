907e79cc883c5c43e7bdef46bdd3c636e61c1b6f
devberry <90882909+devberry@users.noreply.github.com>
2022-01-28 15:18:56 +0900

    Time: 0 ms (100.00%), Space: 43 MB (6.08%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -34,7 +34,9 @@ sol
 1.
 array가 정렬되어 있고 O(logn)의 시간복잡도를 가지게 하면서 target을 찾으려면 이분 탐색이 필요하다.
 다른 부분은 기존의 이분 탐색대로 구현하면 된다.
-target을 찾지 못하게 되면 nums[mid]보다 target이 크면 mid + 1을, 작으면 mid - 1을 반환하도록 한다.
+start와 end가 교차되었을 때 while문에서 빠져나오게 되고,
+while문에서 나오기 직전에 target이 nums[mid]보다 컸다면 mid 바로 다음 위치에 target이 들어가야 하므로 start,
+작았다면 mid 바로 이전 위치에 target이 들어가야 하지만 배열의 인덱스는 0이 최솟값이므로 여전히 start가 반환되어야 한다.
 
 time O(logn)
 space O(1)
