e17e52aeba763573c3753d5d0d02cec6939a120e
devberry <90882909+devberry@users.noreply.github.com>
2022-01-28 14:39:52 +0900

    Time: 30 ms (12.50%), Space: 40.9 MB (10.06%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,24 +3,17 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int start = 1;
-        int end = n;
-        
-        while(start <= end) {
-            int mid = start + (end - start) / 2;
-            
-            if(!isBadVersion(mid)) {
-                start = mid + 1;
+        int left = 1, right = n, ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // to avoid overflow incase (left+right)>2147483647
+            if (isBadVersion(mid)) {
+                ans = mid; // record mid as current answer
+                right = mid - 1; // try to find smaller version in the left side
             } else {
-                if(!isBadVersion(mid - 1)) {
-                    return mid;
-                } else {
-                    end = mid - 1;
-                }
+                left = mid + 1; // try to find in the right side
             }
         }
-        
-        return -1;
+        return ans;
     }
 }
 
