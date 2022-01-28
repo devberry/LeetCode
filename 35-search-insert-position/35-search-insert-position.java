60be0bf0af6b1ac5a5dec6faebc05cbcf282617b
devberry <90882909+devberry@users.noreply.github.com>
2022-01-28 15:09:05 +0900

    Time: 0 ms (100.00%), Space: 43.1 MB (6.08%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -15,11 +15,7 @@ class Solution {
             }
         }
         
-        int mid = (start + end) / 2;
-        if(target > nums[mid]) {
-            return mid + 1;
-        }
-        return mid - 1 < 0 ? 0 : mid - 1;
+        return start;
     }
 }
 
