c4ac86468e72e219d7fc517418790adaf3c03e41
devberry <90882909+devberry@users.noreply.github.com>
2022-02-23 12:55:20 +0900

    Time: 6 ms (67.16%), Space: 46.5 MB (39.97%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.java
@@ -1,39 +1,38 @@
 class Solution {
     public String[] reorderLogFiles(String[] logs) {
-        //log 분류(letter, digit)
+        List<String> letter = new ArrayList<>();
+        List<String> digit = new ArrayList<>();
         
-        List<String> dig = new ArrayList<>();
-        List<String> let = new ArrayList<>();
-        
-        for(int i = 0; i < logs.length; i++) {
-            String[] log = logs[i].split(" ");
+        for(String log : logs) {
+            String[] word = log.split(" ");
             
-            if(Character.isDigit(log[1].charAt(0)))
-                dig.add(logs[i]);
+            if(Character.isDigit(word[1].charAt(0)))
+                digit.add(log);
             else
-                let.add(logs[i]);
+                letter.add(log);
         }
         
-        Collections.sort(let, new Comparator<String>() {
+        Collections.sort(letter, new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
                 String temp1 = s1.substring(s1.indexOf(" ") + 1);
                 String temp2 = s2.substring(s2.indexOf(" ") + 1);
                 
-                if(temp1.equals(temp2))
+                if(temp1.equals(temp2)) {
                     return s1.split(" ")[0].compareTo(s2.split(" ")[0]);
-                else
+                } else {
                     return temp1.compareTo(temp2);
+                }
             }
         });
+            
+        letter.addAll(digit);
         
-        let.addAll(dig);
-        
-        return let.toArray(new String[let.size()]);
-        
+        return letter.toArray(new String[letter.size()]);
     }
 }
 
 /*
-
+time O(n^2*logn) n==logs.length
+space O(mn) m==logs[i].length
 */
